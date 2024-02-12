091868e31f0cba110f7c46bf8ed25a4863e887e5
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-02-12 00:41:57 -0500

    Add autoSwipe check and delay time logging

--- content.js
+++ content.js
@@ -7,23 +7,23 @@ function sleep(ms) {
 
 async function autoSwipeRight() {
   while (true) {
-    const likeButton = document.getElementsByClassName(
-      "button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a"
-    )[0];
-    if (likeButton) {
-      likeButton.click();
-      console.log("Liked");
-    }
     // Infinite loop
     // Check if autoSwipe is enabled and get delay time
     const { autoSwipe, delayTime } = await new Promise((resolve) =>
       chrome.storage.local.get(["autoSwipe", "delayTime"], resolve)
     );
-
+    console.log("Content:", autoSwipe, delayTime);
+    const likeButton = document.getElementsByClassName(
+      "button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a"
+    )[0];
+    if (likeButton && autoSwipe) {
+      likeButton.click();
+      console.log("Liked");
+    }
     // if (!autoSwipe) break; // Exit loop if autoSwipe is disabled
 
     // Wait for the specified delay time before the next action
-    await sleep(delayTime * 1000);
+    await sleep(parseInt(delayTime, 10) * 1000);
   }
 }
 
--- popup.js
+++ popup.js
@@ -3,7 +3,5 @@ console.log("Popup script loaded");
 document.getElementById("saveSettings").addEventListener("click", () => {
   const autoSwipe = document.getElementById("autoSwipe").checked;
   const delayTime = document.getElementById("delayTime").value;
-  chrome.storage.local.set({ autoSwipe, delayTime }, () => {
-    console.log("Settings saved");
-  });
+  chrome.storage.local.set({ autoSwipe, delayTime });
 });
